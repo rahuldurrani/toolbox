016bd68bc2499b190f91ad5d8930eb877b9896b9
shravnk <brendan.ode4@gmail.com>
2018-10-21 03:08:09 -0400

    popovers, history

--- client-app/src/desktop/common/SampleTreeGrid.js
+++ client-app/src/desktop/common/SampleTreeGrid.js
@@ -118,7 +118,7 @@ class SampleTreeGrid extends Component {
             dimensionChooser({
                 model: treeModel,
                 field: 'groupBy',
-                dimensions: ['fund', 'portfolio', 'assetClass', 'security']
+                dimensions: ['fund', 'portfolio', 'trader', 'assetClass']
             })
         )
     }
